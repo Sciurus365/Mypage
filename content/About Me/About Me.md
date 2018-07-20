fe01e2ebdf03466795a8422fe829f9833fc08aee
Jingmeng Cui <942862946@qq.com>
2018-07-20 17:56:00 +0800

    Initial commit

---
+++ content/About Me/About Me.md
@@ -0,0 +1,18 @@
+Title: About Me
+Date: 2018-07-18 17:00
+Modified: 2018-07-20 18:00
+Tags: myself
+
+<img src="http://ou5l217m4.bkt.clouddn.com/18-7-18/48843638.jpg" width=256 />
+
+I am an undergraduate student at [Peking University (PKU)](http://www.pku.edu.cn/) and I am expected to graduate in July, 2019. Squirrels are my favorite animals, and friends usually call me "松鼠"(squirrel).
+
+My two majors are **material chemistry** and **psychology**. I currently work as an intern student at Laboratory of Developmental and Comparative Psychology, [School of Psychology and Cognitive Science (SPCS)](https://www.psy.pku.edu.cn/), PKU. My research interests include interpersonal relationship development of youths,  psychology of homosexuals, and usage of novel statistical methods in psychological research.  I am planning to apply for a research master's program after my graduation. Please contact me if you are interested.
+
+I like playing volleyball. I am a blocker in the men's volleyball team of College of Chemistry and Molecular Engineering, PKU. I am also an entry-level harmonica player. I also like listen to [xiangsheng (crosstalk)](https://en.wikipedia.org/wiki/Xiangsheng) and music. My favorite xiangsheng actor is Degang Guo (郭德纲). My favorite singers are Shen Zhou (周深), Jian Li (李健) and Bella Yao (姚贝娜). 
+
+I am the founder and the first leader of Chemistry Olympiad Breakthrough Program (COBP), an unofficial community for Chinese Chemistry Olympiad competitors. This organization started in Jan. 17th, 2015, and is now maintained by COBP management team.
+
+Here is my [Zhihu homepage](https://www.zhihu.com/people/cui-jing-meng-2/activities), and my WeChat subscription account is "松鼠的窝" (squirrel's nest).
+
+Please contact me though email: cuijm@pku.edu.cn
---
+++ pelicanconf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Jingmeng CUI'
+SITENAME = 'Jingmeng CUI'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'blue-penguin'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# all the following settings are *optional*
+
+# HTML metadata
+SITEDESCRIPTION = ''
+
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME   = True
+DISPLAY_MENU   = True
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+    ('GitHub', 'https://github.com/Sciurus365'),
+)
